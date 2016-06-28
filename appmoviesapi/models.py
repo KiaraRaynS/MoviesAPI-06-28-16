1115e2a797b04332ba1cd2b6afa6ed8cf493181e
KiaraRaynS <sherman.rayn@outlook.com>
2016-06-28 14:32:49 -0400

    Models created

--- appmoviesapi/models.py
+++ appmoviesapi/models.py
@@ -1,3 +1,41 @@
 from django.db import models
 
-# Create your models here.
+
+class Rater(models.Model):
+    age = models.IntegerField()
+    gender = models.CharField(max_length=1)
+    occupation = models.CharField(max_length=30)
+    zip_code = models.CharField(max_length=20)
+
+
+class Movie(models.Model):
+    title = models.CharField(max_length=30)
+    release_date = models.CharField(max_length=20)
+    video_release = models.CharField(max_length=20, null=True)
+    imdb = models.CharField(max_length=120)
+    unknown = models.IntegerField()
+    action = models.IntegerField()
+    adventure = models.IntegerField()
+    animation = models.IntegerField()
+    childrens = models.IntegerField()
+    comedy = models.IntegerField()
+    crime = models.IntegerField()
+    documentary = models.IntegerField()
+    drama = models.IntegerField()
+    fantasy = models.IntegerField()
+    film_noir = models.IntegerField()
+    horror = models.IntegerField()
+    music = models.IntegerField()
+    mystery = models.IntegerField()
+    romance = models.IntegerField()
+    scifi = models.IntegerField()
+    thriller = models.IntegerField()
+    war = models.IntegerField()
+    western = models.IntegerField()
+
+
+class MovieRating(models.Model):
+    rater = models.ForeignKey(Rater)
+    movie = models.ForeignKey(Movie)
+    ratings = models.IntegerField()
+    timestamp = models.IntegerField()
