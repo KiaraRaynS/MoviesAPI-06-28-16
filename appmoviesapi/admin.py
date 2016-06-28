22c5f66bb122b1c62cd1263dc150341cee69237c
KiaraRaynS <sherman.rayn@outlook.com>
2016-06-28 15:56:34 -0400

    Move and Rater create fields added

--- appmoviesapi/admin.py
+++ appmoviesapi/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from appmoviesapi.models import Rater, Movie, MovieRating
+
+admin.site.register(Rater)
+admin.site.register(Movie)
+admin.site.register(MovieRating)
